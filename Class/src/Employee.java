750508a74d408005cd3c300831eae4ade50a9e05
AnhQuanT2109A <103103349+AnhQuanT2109A@users.noreply.github.com>
2022-04-18 17:25:17 +0700

    up

--- Class/src/Employee.java
+++ Class/src/Employee.java
@@ -6,6 +6,38 @@ public class Employee {
     private String lastName;
     private int salary;
     public Employee(int ID, String FN, String LN, int S) {
-        id = 
+        id = ID;
+        firstName = FN;
+        lastName = LN;
+        salary = S;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public String getName() {
+        return firstName + lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    public int AnnualSalary() {
+        return salary * 12;
+    }
+    public int raiseSalary(int percent) {
+        return salary += salary * percent;
     }
 }

3f37ca41d2b283bcdd06b4a79f060eeb7b034907
AnhQuanT2109A <103103349+AnhQuanT2109A@users.noreply.github.com>
2022-04-23 00:32:38 +0700

    Quan

--- Task7/src/Line.java
+++ Task7/src/Line.java
@@ -1,98 +1,144 @@
-import java.awt.*;
-import java.util.Arrays;
-
 public class Line {
-    Point begin, end;
-    public Line(int x1, int yi, int x2, int y2) {
-        begin = new Point(x1, y1);
-        end = new Point(x2, y2);
+    private Point begin,end;
+
+    public Line(int x1,int y1,int x2,int y2) {
+        this.begin = new Point(x1,y1);
+        this.end = new Point(x2,y2);
     }
-    public Line (Point begin, Point end) {
+
+    public Line(Point begin, Point end) {
         this.begin = begin;
         this.end = end;
     }
-    public Point getBegin() {
-        return begin;
-    }
-        public Point getEnd() {
-            return end;
-    }
+
     public void setBegin(Point begin) {
         this.begin = begin;
     }
-        public void setEnd (Point end) {
-            this.end = end;
+
+    public void setEnd(Point end) {
+        this.end = end;
     }
+
+    public Point getBegin() {
+        return begin;
+    }
+
+    public Point getEnd() {
+        return end;
+    }
+
     public int getBeginX() {
         return begin.getX();
     }
-    public int setBeginX(int x) {
+    public int getBeginY() {
         return begin.getY();
     }
-    public void getBeginY(int y) {
+
+    public void setBeginX(int x) {
+        begin.setX(x);
+    }
+    public void setBeginY(int y) {
         begin.setY(y);
     }
-    public int[] getBeginXY() {
-        return begin.getXY();
+
+    public void setBeginXY(int x,int y){
+        begin.setX(x);
+        begin.setY(y);
     }
-    public void setBeginXY (int x, int y) {
-        begin.setXY(x, y);
+
+    public int[] getBeginXY(){
+        return  begin.getXY();
     }
+
     public int getEndX() {
         return end.getX();
-    }
-    public void setEndX(int x) {
-        end.setX(x);
     }
     public int getEndY() {
         return end.getY();
     }
+
+    public void setEndX(int x) {
+        end.setX(x);
+    }
     public void setEndY(int y) {
         end.setY(y);
     }
-    public int[] getEndXY() {
-        return end.getXY();
+    public void setEndXY(int x,int y){
+        end.setX(x);
+        end.setY(y);
     }
-    public void setEndXY(int x, int y) {
-        end.setXY(x, y);
+
+    public int[] getEndXY(){
+        return  end.getXY();
     }
+
     public String toString() {
-        return "Line[begin=" + begin + ", end=" + end + "]";
+        return "Line{" +
+                "begin=" + begin +
+                ", end=" + end +
+                '}';
     }
+
     public double getLength() {
         return begin.distance(end);
     }
 
-    public static void main(String[] args) {
-        Line I1 = new Line(1, 2, 3, 4);
-        System.out.println(I1);
-        Line I2 = new Line(new Point(5,6), new Point(7,8));
-        System.out.println(I2);
-
-        I1.setBegin(new Point(11, 12));
-        I1.setEnd(new Point(13, 14));
-        System.out.println(I1);
-        System.out.println("begin is: " + I1.getBegin());
-        System.out.println("end is: " + I1.getEnd());
-
-        I1.setBeginX(21);
-        I1.setBeginy(22);
-        I1.setEndX(23);
-        I1.setEndy (24);
-        System.out.println(I1);
-        System.out.println("begin's x is: " + I1.getBeginX());
-        System.out.println("begin's y is: " + I1.getBegin());
-        System.out.println("end's x is: " + I1.getEndX());
-        System.out.println("End's y is: " + I1.getEndY());
-
-        I1.setBeginXY(31,32);
-        I1.setEndXY(33,34);
-        System.out.println(I1);
-        System.out.println(I1);
-        System.out.println("begin's x and y are: " + Arrays.toString(I1.getBeginXY()));
-        System.out.println("end's x and y are: " + Arrays.toString(I1.getEndXY()));
-
-//        System.out.println("length is: %.2f%n", I1.getLength());
+    public double getGradient(){
+        double x = begin.getX()-end.getX();
+        double y = begin.getY()-end.getY();
+        return Math.atan2(y,x);
     }
 
+    public double distance(int x, int y){
+        double x1=begin.getX();
+        double y1=begin.getY();
+        double x2=end.getX();
+        double y2=end.getY();
+        double t = (y1-y2)*(x-x1) + (x2-x1)*(y-y1);
+        double m = Math.sqrt((y1-y2)*(y1-y2)+(x1-x2)*(x1-x2));
+        double res = Math.abs(t/m);
+        return  res;
     }
+
+    public double distance(Point point){
+        double x1=begin.getX();
+        double y1=begin.getY();
+        double x2=end.getX();
+        double y2=end.getY();
+        double t = (y1-y2)*(point.getX()-x1) + (x2-x1)*(point.getY()-y1);
+        double m = Math.sqrt((y1-y2)*(y1-y2)+(x1-x2)*(x1-x2));
+        double res = Math.abs(t/m);
+        return  res;
+    }
+
+
+    public boolean intersects(Line another){
+        double x1=another.getBeginX();
+        double y1=another.getBeginY();
+        double x2=another.getEndX();
+        double y2=another.getEndY();
+
+        double check1 = (y1-y2)*(begin.getX()-x1) + (x2-x1)*(begin.getY()-y1);
+        double check2 = (y1-y2)*(end.getX()-x1) + (x2-x1)*(end.getY()-y1);
+
+        double x3=begin.getX();
+        double y3=begin.getY();
+        double x4=end.getX();
+        double y4=end.getY();
+
+        double check3 = (y3-y4)*(another.getBeginX()-x3) + (x4-x3)*(another.getBeginY()-y3);
+        double check4 = (y3-y4)*(another.getEndX()-x3) + (x4-x3)*(another.getEndY()-y3);
+
+        if(check1*check2<0 && check3*check4<0){
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
+
+
+
+}

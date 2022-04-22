d5279ffd3665836216440c69075dd80c8ab668af
AnhQuanT2109A <103103349+AnhQuanT2109A@users.noreply.github.com>
2022-04-22 21:33:55 +0700

    Task7

--- Task7/src/Book.java
+++ Task7/src/Book.java
@@ -24,5 +24,34 @@ public class Book {
     }
     public int getQty() {
         return qty;
-    
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public String toString() {
+        return "*" + name + " by " + author;
+    }
+
+    public static void main(String[] args) {
+        Author ahTeck = new Author("Tan Ah Teck", "ahTeck@somewhere.com", 'm');
+        System.out.println(ahTeck);
+        Book dummyBook = new Book("Java for dummies", ahTeck, 9.99, 99);
+        System.out.println(dummyBook);
+        dummyBook.setPrice(8.88);
+        dummyBook.setQty(88);
+        System.out.println("name is: " + dummyBook.getName());
+        System.out.println("price is: " + dummyBook.getPrice());
+        System.out.println("qty is: " + dummyBook.getQty());
+        System.out.println("author is: " + dummyBook.getAuthor());
+        System.out.println("author's name is: " + dummyBook.getAuthor().getName());
+        System.out.println("author's email is: " + dummyBook.getAuthor().getEmail());
+        System.out.println("author's gender is: " + dummyBook.getAuthor().getGender());
+        Book moreDummyBook = new Book("Java for more dummies",
+                new Author("Peter Lee", "peter@nowhere.com", 'm'),
+                19.99, 8);
+        System.out.println(moreDummyBook);
+
+    }
     }

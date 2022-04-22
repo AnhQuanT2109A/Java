6ace175b1ab0a415f7e61bf50d837d1b9034ac67
AnhQuanT2109A <103103349+AnhQuanT2109A@users.noreply.github.com>
2022-04-22 21:37:08 +0700

    BT

--- Task4/src/Student.java
+++ Task4/src/Student.java
@@ -1,48 +1,16 @@
 public class Student {
-    int rollNo;
-    String name;
-    String address;
-    float marks;
+    private String name;
+    private int age;
 
-    public Student() {
-        rollNo = 0;
-        name = "";
-        address = "";
-        marks = 0;
-    }
-    public Student(int rNo, String sname) {
-        rollNo = rNo;
-        name = sname;
-    }
-    public Student(int rNo, float score) {
-        rollNo = rNo;
-       marks = score;
-    }
-    public Student(String sName, String addr) {
-        name = sName;
-        address = addr;
-    }
-    public Student(int rNo, String sname, float score) {
-        rollNo = rNo;
-        name = sname;
-        marks = score;
-    }
-    public void displayDetails() {
-        System.out.println("Rollno: "+ rollNo);
-        System.out.println("Student name: "+name);
-        System.out.println("Address "+address);
-        System.out.println("Score "+marks);
-        System.out.println("--------------------");
+    public Student(String nameN, int ageA) {
+        name = nameN;
+        age = ageA;
     }
 
-    public static void main(String[] args) {
-        Student objStud1 = new Student("David", "302, Washington Street");
-        objStud1.displayDetails();
-        Student objStud2 = new Student(103, 46);
-        objStud2.displayDetails();
-        Student objStud3 = new Student(104,"Roger", 50);
-        objStud3.displayDetails();
+    public String getName() {
+        return name;
     }
-
-
+    public int getAge() {
+        return age;
+    }
 }

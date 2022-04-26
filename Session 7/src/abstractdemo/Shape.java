34b74268e2d0ab3637de6893678f0264561e01d5
AnhQuanT2109A <103103349+AnhQuanT2109A@users.noreply.github.com>
2022-04-27 03:51:50 +0700

    Quan_BT

--- Session 7/src/abstractdemo/Shape.java
+++
@@ -1,9 +0,0 @@
-package abstractdemo;
-
-abstract class Shape {
-    private final float PI = 3.14F;
-    public float getPI() {
-        return PI;
-    }
-    abstract void calculate(float val);
-}
